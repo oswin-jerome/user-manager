4f14c2b961d4fa0f665a007e7a8ddcbdb703fd1a
oswin-jerome <oswinjeromej@gmail.com>
2024-06-12 21:06:45 +0530

    Add logout feature

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
-import { Nav, NavItem } from "reactstrap";
+import { Button, Nav, NavItem } from "reactstrap";
 
 function NavBar() {
   return (
-    <div className="p-4 bg-secondary text-white d-flex justify-content-between">
+    <div className="p-4 bg-secondary text-white d-flex justify-content-between align-items-center">
       <h4>MyApp</h4>
       <Nav className="gap-4">
         <NavItem>
@@ -20,6 +20,16 @@ function NavBar() {
           <Link className="text-white" to={"/login"}>
             Login
           </Link>
+        </NavItem>
+        <NavItem>
+          <Button
+            onClick={() => {
+              localStorage.removeItem("token");
+              window.location.replace("/login");
+            }}
+          >
+            Logout
+          </Button>
         </NavItem>
       </Nav>
     </div>
