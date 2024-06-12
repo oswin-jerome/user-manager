bfb3c1c29d10f2d45bdb9a386f9330ac85fa6eb1
oswin-jerome <oswinjeromej@gmail.com>
2024-06-12 10:08:47 +0530

    Prevent duplicate emails

--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -14,6 +14,17 @@ router.post("/login", (req, res) => {
 
 router.post("/register", async (req, res) => {
   const data = req.body;
+
+  const checkUser = await User.findOne({
+    email: data.email,
+  });
+
+  if (checkUser) {
+    return res.status(422).send({
+      message: "Email already exist",
+    });
+  }
+
   const user = new User(data);
   const doc = await user.save();
   res.send(doc);
