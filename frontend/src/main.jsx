f8a36fcdb53145407561e4b5a23f8c78c38a1f40
oswin-jerome <oswinjeromej@gmail.com>
2024-06-12 21:04:15 +0530

    Home Page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,7 @@ import AboutPage from "./pages/About.jsx";
 import LoginPage from "./pages/Login.jsx";
 import NavBar from "./components/NavBar.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import HomePage from "./pages/HomePage.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -17,7 +18,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <div className="p-4">
         <Routes>
           <Route path="/" element={<ProtectedRoute />}>
-            <Route path="/" element={<AboutPage />} />
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
           </Route>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <div className="container mt-4">
+      <h1>Welcome to MyApp</h1>
+    </div>
+  );
+}
+
+export default HomePage;
