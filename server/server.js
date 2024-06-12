41d6e90f083c3b475df5cac8e24e5dbe84253be9
oswin-jerome <oswinjeromej@gmail.com>
2024-06-12 09:50:32 +0530

    setup routes

--- server/server.js
+++ server/server.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const authRoute = require("./routes/authRoute");
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  console.log("res");
-
-  res.send("Working....");
-});
+app.use("/auth", authRoute);
 
 app.listen(5500, () => {
   console.log("Server Started....");
