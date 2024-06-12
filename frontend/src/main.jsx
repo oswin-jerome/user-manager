ba96ff696ad2a8c45376f8cad853330091b0f23e
oswin-jerome <oswinjeromej@gmail.com>
2024-06-12 11:33:36 +0530

    Register form

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home.jsx";
 import AboutPage from "./pages/About.jsx";
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,33 @@
+import { Button, Card, CardBody, CardHeader, CardTitle, Form, FormGroup, Input, Label } from "reactstrap";
+
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className="container m-5">
+      <Card>
+        <CardHeader>
+          <CardTitle tag="h5">Register</CardTitle>
+        </CardHeader>
+        <CardBody>
+          <Form>
+            <FormGroup>
+              <Label>Name</Label>
+              <Input />
+            </FormGroup>
+            <FormGroup>
+              <Label>Email</Label>
+              <Input type="email" />
+            </FormGroup>
+            <FormGroup>
+              <Label>Password</Label>
+              <Input type="password" />
+            </FormGroup>
+
+            <Button>Register</Button>
+          </Form>
+        </CardBody>
+      </Card>
+    </div>
+  );
 }
 
 export default HomePage;
