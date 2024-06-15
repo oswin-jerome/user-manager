c67504dfa78621993b30e6b165e78a0a37029fa9
oswin-jerome <oswinjeromej@gmail.com>
2024-06-15 11:27:09 +0530

    Hide menus items based on login status

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,6 +2,8 @@ import { Link } from "react-router-dom";
 import { Button, Nav, NavItem } from "reactstrap";
 
 function NavBar() {
+  const token = localStorage.getItem("token");
+
   return (
     <div className="p-4 bg-secondary text-white d-flex justify-content-between align-items-center">
       <h4>MyApp</h4>
@@ -11,26 +13,32 @@ function NavBar() {
             Home
           </Link>
         </NavItem>
-        <NavItem>
-          <Link className="text-white" to={"/register"}>
-            Register
-          </Link>
-        </NavItem>
-        <NavItem>
-          <Link className="text-white" to={"/login"}>
-            Login
-          </Link>
-        </NavItem>
-        <NavItem>
-          <Button
-            onClick={() => {
-              localStorage.removeItem("token");
-              window.location.replace("/login");
-            }}
-          >
-            Logout
-          </Button>
-        </NavItem>
+        {!token && (
+          <NavItem>
+            <Link className="text-white" to={"/register"}>
+              Register
+            </Link>
+          </NavItem>
+        )}
+        {!token && (
+          <NavItem>
+            <Link className="text-white" to={"/login"}>
+              Login
+            </Link>
+          </NavItem>
+        )}
+        {token && (
+          <NavItem>
+            <Button
+              onClick={() => {
+                localStorage.removeItem("token");
+                window.location.replace("/login");
+              }}
+            >
+              Logout
+            </Button>
+          </NavItem>
+        )}
       </Nav>
     </div>
   );
